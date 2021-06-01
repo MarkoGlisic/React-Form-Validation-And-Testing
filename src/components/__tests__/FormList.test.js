ff20d40ff0b0c72cdfd14ff65c4765c6e38fd399
Marko Glisic <38302481+MarkoGlisic@users.noreply.github.com>
2021-06-01 21:06:14 +0200

    added snapshot test

--- src/components/__tests__/FormList.test.js
+++ src/components/__tests__/FormList.test.js
@@ -1,17 +1,19 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import FormList from "../FormList.js";
-import Form from "react-bootstrap";
+import renderer from "react-test-renderer";
+
+afterEach(cleanup);
 
 describe("<FormList/>", () => {
   // Email Input Tests
-  test("renders email input", () => {
+  it("renders email input", () => {
     render(<FormList />);
     const inputEl = screen.getByTestId("email-input");
     expect(inputEl).toBeInTheDocument();
     expect(inputEl).toHaveAttribute("type", "email");
   });
-  test("pass a valid email to email input field", () => {
+  it("pass a valid email to email input field", () => {
     render(<FormList />);
     const inputEl = screen.getByTestId("email-input");
     userEvent.type(inputEl, "test@test.com");
@@ -19,13 +21,13 @@ describe("<FormList/>", () => {
   });
 
   // Name Input Tests
-  test("renders name input", () => {
+  it("renders name input", () => {
     render(<FormList />);
     const inputEl = screen.getByTestId("name-input");
     expect(inputEl).toBeInTheDocument();
     expect(inputEl).toHaveAttribute("type", "text");
   });
-  test("pass a valid name to name input field", () => {
+  it("pass a valid name to name input field", () => {
     render(<FormList />);
     const inputEl = screen.getByTestId("name-input");
     userEvent.type(inputEl, "John");
@@ -33,16 +35,22 @@ describe("<FormList/>", () => {
   });
 
   // Age Input Tests
-  test("renders name input", () => {
+  it("renders name input", () => {
     render(<FormList />);
     const inputEl = screen.getByTestId("age-input");
     expect(inputEl).toBeInTheDocument();
     expect(inputEl).toHaveAttribute("type", "text");
   });
-  test("pass a valid age to age input field", () => {
+  it("pass a valid age to age input field", () => {
     render(<FormList />);
     const inputEl = screen.getByTestId("age-input");
     userEvent.type(inputEl, "25");
     expect(screen.getByTestId("age-input")).toHaveValue("25");
+  });
+
+  // Snapshots
+  it("matches the snapshot", () => {
+    const tree = renderer.create(<FormList />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
